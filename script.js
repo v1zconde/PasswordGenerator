f708582c847e9948af6ea296682b4e84c10d6cb0
rvasquez <rvasquez1@gmail.com>
2020-12-17 18:24:00 -0500

    moving the variables to outside if they can be use globally

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// Assignment Code
+// Variables
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 var lowerArray = arrayRandomsLowToHigh(97,122);
 var upperArray = arrayRandomsLowToHigh(65,90);
 var numberArray = arrayRandomsLowToHigh(48,57);
 var symbolArray = arrayRandomsLowToHigh(33,47);
-
+var passwordfinal =[];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 // Write password to the #password input
 function writePassword() {
@@ -19,23 +18,16 @@ function writePassword() {
   var numberChar = confirm("Click confirm to start number Char");
   var upperChar = confirm("Click confirm to start upper Char");
   
-  
-  console.log(passLenght);
-  console.log(symbolChar);
-  console.log(numberChar);
-  console.log(upperChar);
-
   var password = generatePassword(passLenght, symbolChar, numberChar, upperChar);
  
-  //console.log(passwordfinal);
+  //print the password to the DOM
   passwordText.value = passwordfinal;
  
 }
 
 function generatePassword(passLenght, symbolChar, numberChar, upperChar) {
  
-  
-   var optionsPass = lowerArray;
+  var optionsPass = lowerArray;
   console.log(optionsPass);
 
   if (upperChar) optionsPass = optionsPass.concat(upperArray);
@@ -43,20 +35,15 @@ function generatePassword(passLenght, symbolChar, numberChar, upperChar) {
   if (symbolChar) optionsPass = optionsPass.concat(symbolArray);
   
   console.log(optionsPass);
-
-  var passwordfinal =[];
-
+  
   for (var i = 0; i < passLenght; i++){
 
     var passcode = optionsPass[Math.floor(Math.random() * optionsPass.length)];
     passwordfinal.push(String.fromCharCode(passcode));
-
     console.log(passwordfinal);
     
-  }
-  
+  } 
    return passwordfinal.join("");
-
 }
 
 function arrayRandomsLowToHigh(low,high){
